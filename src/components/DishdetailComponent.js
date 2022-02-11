8ac7f1741b8608d828dffca08d438d2d093923ea
nhimdauheo <hieunguyen479@gmail.com>
2022-02-11 11:10:16 +0700

    Exc1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
+import dateFormat from "dateformat"
+
+class Dishdetail extends Component {
+    constructor(props) {
+        super(props)
+    }
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <div className="col-12 m-1">
+                    <div className="row">
+                        <div className="col-5 col-md-5 p-1">
+                            <Card>
+                                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-5 col-md-5 p-1">
+                            <Card>
+                                <h4>Comments</h4>
+                                {dish.comments.map((cmt) => {
+                                    return (
+                                        <CardBody>
+                                            <CardText>{cmt.comment}</CardText>
+                                            <CardText>--{cmt.author}, {dateFormat(new Date(cmt.date), "mmm d, yyyy")}</CardText>
+                                        </CardBody>
+                                    )
+                                })}
+
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div></div>
+            )
+        }
+    }
+    render() {
+        return this.renderDish(this.props.dish);
+    }
+}
+
+export default Dishdetail
---
+++ src/components/MenuComponents.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import Dishdetail from "./DishdetailComponent";
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDish : null
+        }
+    }
+    onDishSelected(dish){
+        this.setState({selectedDish: dish})
+    }
+
+    //Hiện thị dữ liệu
+    render() {
+        const menu = this.props.dishes.map((dish)=>{
+            return (
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=>this.onDishSelected(dish)}>
+                            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            )
+        });
+        return (
+            <div className="container">
+                <div className="row">
+                        {menu}
+                </div>
+                <div className="row">
+                    <Dishdetail
+                    dish = {this.state.selectedDish}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+export default Menu;
