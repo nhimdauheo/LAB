8bf26af4645b82e05be7138d0198dbe7d7a159f2
nhimdauheo <hieunguyen479@gmail.com>
2022-02-13 18:53:45 +0700

    Box

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,30 +9,33 @@ class Dishdetail extends Component {
     renderDish(dish) {
         if (dish != null) {
             return (
-                <div className="col-12 col-5 m-1">
+                <div className="container">
                     <div className="row">
-                        <div className="col-6 col-md-4 p-1">
-                            <Card>
-                                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-6 col-md-4 p-1">
-                            <Card>
-                                <h4>Comments</h4>
-                                {dish.comments.map((cmt) => {
-                                    return (
+                        <div className="col-12 col-5 m-1">
+                            <div className="row">
+                                <div className="col-5 col-5 m-1">
+                                    <Card>
+                                        <CardImg width="100%" src={dish.image} alt={dish.name} />
                                         <CardBody>
-                                            <CardText>{cmt.comment}</CardText>
-                                            <CardText>--{cmt.author}, {dateFormat(new Date(cmt.date), "mmm d, yyyy")}</CardText>
+                                            <CardTitle>{dish.name}</CardTitle>
+                                            <CardText>{dish.description}</CardText>
                                         </CardBody>
-                                    )
-                                })}
-
-                            </Card>
+                                    </Card>
+                                </div>
+                                <div className="col-5 col-5 m-1">
+                                    <Card>
+                                        <h4>Comments</h4>
+                                        {dish.comments.map((cmt) => {
+                                            return (
+                                                <CardBody>
+                                                    <CardText>{cmt.comment}</CardText>
+                                                    <CardText>--{cmt.author}, {dateFormat(new Date(cmt.date), "mmm d, yyyy")}</CardText>
+                                                </CardBody>
+                                            )
+                                        })}
+                                    </Card>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
