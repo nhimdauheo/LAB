7e934aabc8f4dfaef822d03c13e04501c9321f38
nhimdauheo <hieunguyen479@gmail.com>
2022-02-23 14:16:43 +0700

    Ex2

--- src/components/MainCompomponent.js
+++ src/components/MainCompomponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Menu from './MenuComponents';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Dishdetail from "./DishdetailComponent";
 import Header from './HeaderComponent';
@@ -46,7 +47,8 @@ class Main extends Component {
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
           <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path='/contactus' component={Contact} /> 
+          <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} /> 
           <Redirect to="/home" />
        </Switch>
       <Footer/>
